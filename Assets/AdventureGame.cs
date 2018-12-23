f8d69c94dc788c6ce929c44066e091f0199c4325
EZ3Z <30964083+EZ3Z@users.noreply.github.com>
2018-12-22 23:19:54 -0700

    Added GetState functionality

--- Assets/AdventureGame.cs
+++ Assets/AdventureGame.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -6,16 +7,41 @@ using UnityEngine.UI;
 public class AdventureGame : MonoBehaviour
 {
     [SerializeField] Text textComponent;
+    [SerializeField] State startingState;
+
+    State state;
     // Start is called before the first frame update
     void Start()
     {
-        //textComponent.text = "AMERICA used to be great but now is super shitty. You are a 71-year-old reality TV star named Donald Trump. What do you do? \r\n\n1. Run for president.\r\n2. Enjoy the rest of your life.You are a celebrity.";
-        textComponent.text = "this was created by a script";
+        state = startingState;
+        textComponent.text = state.GetStateStory();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        ManageState();
+    }
+    private void ManageState()
+    {
+        var nextStates = state.GetNextStates();
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            state = nextStates[0];
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            state = nextStates[1];
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            state = nextStates[2];
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            state = nextStates[3];
+        }
+
+        textComponent.text = state.GetStateStory();
     }
 }
